a6eda7451ee0ae7a35c776eab876788881301904
Geoblin <Oscar_niklasson94@hotmail.com>
2021-12-07 14:36:21 +0100

    first commit

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
+    //Klickar på ingrediens och ändra på ingredients
+    this.subscription = this.slService.startedEditing.subscribe((index: number) =>{
+      //aktiverar klicket på ingredients
+      this.editedItemIndex = index;
+      this.editMode = true;
+      //editar ingredients
+      this.editedItem = this.slService.getIngredient(index);
+      this.slForm.setValue({
+        name: this.editedItem.name,
+        amount: this.editedItem.amount
+      })
+    });
+  }
+  //lägg till item in shopping listan
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    //kollar om editmode är på och uppdaterar eller lägger till ingredient
+    if (this.editMode){
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      console.log("första")
+    }else{
+      this.slService.addIngredient(newIngredient);
+      console.log("andra")
+    }
+    this.editMode = false;
+    form.reset();
+  }
+  //clearbutton method
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+  //deletebutton method + clear
+  onDelete(){
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+}
